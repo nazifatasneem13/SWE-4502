828a0962203c08e66be7d5bc01ee6cf4dc71e545
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 00:00:47 +0600

    imported json

--- FindWeather/api/OpenWeatherAPI.java
+++ FindWeather/api/OpenWeatherAPI.java
@@ -1,5 +1,9 @@
 package api;
-
+import org.json.JSONObject;
 public class OpenWeatherAPI {
     private static final String API_KEY = "0e87b701dc735ede819b1fed3ad63aeb";
+    public JSONObject getWeather()
+    {
+        return new JSONObject();
+    }
 }
