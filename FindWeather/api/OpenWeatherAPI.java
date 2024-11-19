8a48754ba2b6f81fd8c892926a04ae7cc5427897
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 00:37:21 +0600

    exception add

--- FindWeather/api/OpenWeatherAPI.java
+++ FindWeather/api/OpenWeatherAPI.java
@@ -3,16 +3,16 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.*;
+import java.util.Scanner;
 
 public class OpenWeatherAPI {
     private static final String API_KEY = "0e87b701dc735ede819b1fed3ad63aeb";
-    public JSONObject getWeatherByCity(String city)
-    {
+    public JSONObject getWeatherByCity(String city) throws Exception {
         String apiUrl = String.format(
                 "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=%s",
                 city, API_KEY
         );
-        return fetchWeatherData();
+        return fetchWeatherData(apiUrl);
     }
     public JSONObject getWeatherByIP(String city)
     {
@@ -22,6 +22,8 @@ public class OpenWeatherAPI {
         URL url = new URL(apiUrl);
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
+
+        Scanner scanner = new Scanner(conn.getInputStream());
         return new JSONObject();
     }
 }
