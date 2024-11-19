d0c82050cf248885e7c4107434bfaf0fc6657a25
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 01:31:42 +0600

    add cache entry class

--- FindWeather/utils/CacheManager.java
+++ FindWeather/utils/CacheManager.java
@@ -2,9 +2,13 @@ package utils;
 
 import models.WeatherData;
 
+import java.util.HashMap;
+
 public class CacheManager {
+    private final HashMap<String, CacheEntry> cache = new HashMap<>();
     public WeatherData getCachedData(String location)
     {
         return null;
     }
+    private static class CacheEntry {}
 }
