37be5252b74d42dd0caa9a722b32560a87e15e25
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 00:31:44 +0600

    added url

--- FindWeather/api/OpenWeatherAPI.java
+++ FindWeather/api/OpenWeatherAPI.java
@@ -4,6 +4,10 @@ public class OpenWeatherAPI {
     private static final String API_KEY = "0e87b701dc735ede819b1fed3ad63aeb";
     public JSONObject getWeatherByCity(String city)
     {
+        String apiUrl = String.format(
+                "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=%s",
+                city, API_KEY
+        );
         return featchWeatherData();
     }
     public JSONObject getWeatherByIP(String city)
