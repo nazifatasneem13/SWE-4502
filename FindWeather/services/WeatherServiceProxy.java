818e351733378b69ee99edc5f404b6ed490cb9bd
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 02:29:16 +0600

    updated proxy class

--- FindWeather/services/WeatherServiceProxy.java
+++ FindWeather/services/WeatherServiceProxy.java
@@ -1,4 +1,11 @@
 package services;
+import adapter.OpenWeatherAdapter;
+import utils.CacheManager;
 public class WeatherServiceProxy{
-
+    private final OpenWeatherAdapter openWeather;
+    private final CacheManager cacheManager;
+    public WeatherServiceProxy() {
+        this.openWeather = new OpenWeatherAdapter();
+        this.cacheManager = new CacheManager();
+    }
 }
