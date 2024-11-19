3e8b13b93494f8a58dd1a563e4abfc22f968d76d
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 00:29:42 +0600

    update func

--- FindWeather/api/OpenWeatherAPI.java
+++ FindWeather/api/OpenWeatherAPI.java
@@ -2,16 +2,16 @@ package api;
 import org.json.JSONObject;
 public class OpenWeatherAPI {
     private static final String API_KEY = "0e87b701dc735ede819b1fed3ad63aeb";
-    public JSONObject getWeather()
-    {
-        return new JSONObject();
-    }
     public JSONObject getWeatherByCity(String city)
     {
-
+        return featchWeatherData();
+    }
+    public JSONObject getWeatherByIP(String city)
+    {
+        return featchWeatherData();
     }
     public JSONObject featchWeatherData()
     {
-        
+        return new JSONObject();
     }
 }
