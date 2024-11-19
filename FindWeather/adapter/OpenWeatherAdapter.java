dab89b10b22a74726f1b1459b4d9fec051d80d19
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 00:53:52 +0600

    added constructor

--- FindWeather/adapter/OpenWeatherAdapter.java
+++ FindWeather/adapter/OpenWeatherAdapter.java
@@ -1,14 +1,20 @@
 package adapter;
-
+import api.OpenWeatherAPI;
 import models.WeatherData;
+import org.json.JSONObject;
 
 public class OpenWeatherAdapter implements WeatherProvider{
+    private final OpenWeatherAPI api;
+    public OpenWeatherAdapter() {
+        this.api = new OpenWeatherAPI();
+    }
     @Override
     public WeatherData getWeatherByIP(double latitude, double longitude) throws Exception {
         return new WeatherData();
     }
     @Override
     public WeatherData getWeatherByCity(String city) throws Exception {
+
         return new WeatherData();
     }
 }
