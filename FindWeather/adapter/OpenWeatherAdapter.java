362185f9c49a0489e3a32632f2ac2446049ebf19
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 00:43:43 +0600

    update adapter

--- FindWeather/adapter/OpenWeatherAdapter.java
+++ FindWeather/adapter/OpenWeatherAdapter.java
@@ -1,5 +1,11 @@
 package adapter;
 
+import models.WeatherData;
+
 public class OpenWeatherAdapter implements WeatherProvider{
+    @Override
+    public WeatherData getWeatherByIP(double latitude, double longitude) throws Exception {
+        return new WeatherData();
+    }
     
 }
