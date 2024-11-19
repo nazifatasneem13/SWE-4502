969e9ce303168c488b9a1a59b82c771f841c0de3
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 00:35:22 +0600

    created http and fixed some errors

--- FindWeather/api/OpenWeatherAPI.java
+++ FindWeather/api/OpenWeatherAPI.java
@@ -1,5 +1,9 @@
 package api;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.*;
+
 public class OpenWeatherAPI {
     private static final String API_KEY = "0e87b701dc735ede819b1fed3ad63aeb";
     public JSONObject getWeatherByCity(String city)
@@ -8,14 +12,16 @@ public class OpenWeatherAPI {
                 "https://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=%s",
                 city, API_KEY
         );
-        return featchWeatherData();
+        return fetchWeatherData();
     }
     public JSONObject getWeatherByIP(String city)
     {
-        return featchWeatherData();
+        return fetchWeatherData();
     }
-    public JSONObject featchWeatherData()
-    {
+    public JSONObject fetchWeatherData(String apiUrl) throws Exception {
+        URL url = new URL(apiUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
         return new JSONObject();
     }
 }
