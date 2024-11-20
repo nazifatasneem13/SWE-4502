943ff31fe4845768e59be8d4ebed38b262ad59d7
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 20:59:54 +0600

    add

--- FindWeather/adapter/WeatherStackAdapter.java
+++ FindWeather/adapter/WeatherStackAdapter.java
@@ -1,8 +1,14 @@
 package adapter;
-
+import api.WeatherStackAPI;
 import models.WeatherData;
 
 public class WeatherStackAdapter implements WeatherProvider{
+    private final WeatherStackAPI api;
+    public WeatherStackAdapter() {
+        this.api = new WeatherStackAPI();
+    }
     @Override
     public WeatherData getWeatherByCity(String city) throws Exception {}
+    @Override
+    public WeatherData getWeatherByIP(double latitude, double longitude) throws Exception {}
 }
