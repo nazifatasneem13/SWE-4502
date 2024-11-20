b126e69f3ebca4c14c3bcb0aeab37352358b4610
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 20:54:13 +0600

    add

--- FindWeather/api/WeatherStackAPI.java
+++ FindWeather/api/WeatherStackAPI.java
@@ -5,6 +5,13 @@ import org.json.JSONObject;
 public class WeatherStackAPI {
     private static final String API_KEY = "f1177d07fc4272ae4bbccb419a699e91";
     public JSONObject getWeatherByCity(String city) throws Exception {
+        String apiUrl = String.format(
+                "http://api.weatherstack.com/current?access_key=%s&query=%s",
+                API_KEY, city
+        );
         return fetchWeatherData(apiUrl);
+    }
+    private JSONObject fetchWeatherData(String apiUrl) throws Exception {
+
     }
 }
