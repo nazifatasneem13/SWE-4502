38912783cb96a254735d0e47d4643d8115839b2b
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-21 03:07:07 +0600

    ipstackservice

--- FindWeather/api/IPStackService.java
+++ FindWeather/api/IPStackService.java
@@ -1,5 +1,7 @@
 package api;
 
+import models.Location;
 public class IPStackService {
-
+    private static final String API_KEY = "672a2c4fe1dd4739cf51e155cec02b4f";
+    public Location getLocationByIP() throws Exception {}
 }
