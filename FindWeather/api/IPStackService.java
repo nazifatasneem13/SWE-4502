b2b66757764f649be86c80f1629947be20fc5f51
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-21 03:07:56 +0600

    getlocationbyIP

--- FindWeather/api/IPStackService.java
+++ FindWeather/api/IPStackService.java
@@ -1,7 +1,17 @@
 package api;
 
 import models.Location;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class IPStackService {
     private static final String API_KEY = "672a2c4fe1dd4739cf51e155cec02b4f";
-    public Location getLocationByIP() throws Exception {}
+    public Location getLocationByIP() throws Exception {
+        String apiUrl = "http://api.ipstack.com/check?access_key=" + API_KEY;
+
+        URL url = new URL(apiUrl);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+    }
 }
