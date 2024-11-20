a73c83261154c2668da266ce6c4031ce96745d1b
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 21:00:41 +0600

    json object

--- FindWeather/adapter/WeatherStackAdapter.java
+++ FindWeather/adapter/WeatherStackAdapter.java
@@ -1,6 +1,7 @@
 package adapter;
 import api.WeatherStackAPI;
 import models.WeatherData;
+import org.json.JSONObject;
 
 public class WeatherStackAdapter implements WeatherProvider{
     private final WeatherStackAPI api;
@@ -8,7 +9,11 @@ public class WeatherStackAdapter implements WeatherProvider{
         this.api = new WeatherStackAPI();
     }
     @Override
-    public WeatherData getWeatherByCity(String city) throws Exception {}
+    public WeatherData getWeatherByCity(String city) throws Exception {
+        JSONObject response = api.getWeatherByCity(city);
+        double temperature = response.getJSONObject("current").getDouble("temperature");
+        String conditions = response.getJSONObject("current").getJSONArray("weather_descriptions").getString(0);
+    }
     @Override
     public WeatherData getWeatherByIP(double latitude, double longitude) throws Exception {}
 }
