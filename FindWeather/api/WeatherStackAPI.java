668c3229494a7a8d6b29a73908e5c4ad16dd235a
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-20 20:51:36 +0600

    add

--- FindWeather/api/WeatherStackAPI.java
+++ FindWeather/api/WeatherStackAPI.java
@@ -1,5 +1,10 @@
 package api;
 
+import org.json.JSONObject;
+
 public class WeatherStackAPI {
     private static final String API_KEY = "f1177d07fc4272ae4bbccb419a699e91";
+    public JSONObject getWeatherByCity(String city) throws Exception {
+        return fetchWeatherData(apiUrl);
+    }
 }
