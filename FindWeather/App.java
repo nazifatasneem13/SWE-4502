fb0996a12813838668ca49ab2b59b2e1947677fe
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-21 22:28:34 +0600

    Add

--- FindWeather/App.java
+++ FindWeather/App.java
@@ -1,5 +1,9 @@
+import services.WeatherServiceProxy;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-
+        WeatherServiceProxy weatherService = new WeatherServiceProxy();
+        Scanner scanner = new Scanner(System.in);
     }
 }
