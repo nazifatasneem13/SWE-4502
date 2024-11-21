d392cd22f3f476b9e6d9ef1da22a839a1957a03d
Nazifa Tasneem <nazifatasneem.13@gmail.com>
2024-11-21 22:37:19 +0600

    updated app.java

--- FindWeather/App.java
+++ FindWeather/App.java
@@ -5,5 +5,9 @@ public class App {
     public static void main(String[] args) {
         WeatherServiceProxy weatherService = new WeatherServiceProxy();
         Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Weather App Menu:");
+            System.out.println("1. Get Weather by IP");}
     }
 }
